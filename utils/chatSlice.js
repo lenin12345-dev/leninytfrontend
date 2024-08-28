0d87ec70f07f59caebd0a918ea0c9359c09d03f2
lenin12345-dev <leninmohapatra@gmail.com>
2024-08-27 22:45:48 -0400

    google api key change

--- utils/chatSlice.js
+++ utils/chatSlice.js
@@ -1,21 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { LIVE_CHAT_COUNT } from "./constants";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { LIVE_CHAT_COUNT } from "./constants";
 
-const chatSlice = createSlice({
-  name: "chat",
-  initialState: {
-    messages: [],
-  },
-  reducers: {
-    addMessage: (state, action) => {
-      if (state.messages.length>LIVE_CHAT_COUNT){
-        state.messages.splice(0, 1);
+// const chatSlice = createSlice({
+//   name: "chat",
+//   initialState: {
+//     messages: [],
+//   },
+//   reducers: {
+//     addMessage: (state, action) => {
+//       if (state.messages.length>LIVE_CHAT_COUNT){
+//         state.messages.splice(0, 1);
 
-      }
-      state.messages.push(action.payload);
-    },
-  },
-});
+//       }
+//       state.messages.push(action.payload);
+//     },
+//   },
+// });
 
-export const { addMessage } = chatSlice.actions;
-export default chatSlice.reducer;
+// export const { addMessage } = chatSlice.actions;
+// export default chatSlice.reducer;
---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=200&regionCode=IN&key=
+  ${GOOGLE_API_KEY}` ;
+
+export const YOUTUBE_SEARCH_API =
+  `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=
+ ${GOOGLE_API_KEY} &`;
